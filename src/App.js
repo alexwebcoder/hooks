f85083dbe67d6ce13774189fa27931fce0f266d6
alexwebcoder <alexdeleon@stayup.com>
2023-06-19 21:28:04 -0700

    use state hook video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+    useState();
+    //has to be at top level of the function, not in an if block
+    //use state always returns an array with two values
+    //the first one is the current state and the second is the function that updates the current state
+    //if you pass in the initial state as a function, it will run only the first time the component renders
+    const [count, setCount] = useState(()=>{
+
+      console.log('i ran only once');
+      return 4;
+    });
+    const decrementCount = () => {
+      setCount(prevCount => prevCount - 1);
+      setCount(prevCount => prevCount - 1);
+    }
+
+    const increaseCount = () => {
+      setCount(prevCount => prevCount + 1);
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={increaseCount}>+</button>
     </div>
   );
 }
